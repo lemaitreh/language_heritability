7e3a5f626c2a10caa1eefc220a7057c1169ef27c
lemaitreh <15727574+lemaitreh@users.noreply.github.com>
2018-12-05 17:49:12 +0100

    adding some comments

--- scripts/language_network.py
+++ scripts/language_network.py
@@ -5,6 +5,9 @@ out of that.
 Need to install the ibc_public package
 git clone git@github.com:hbp-brain-charting/public_analysis_code.git
 
+Need functions in conjunction.py file
+runfile('./conjunction.py')
+
 Author: Bertrand Thirion, Herve Lemaitre, 2018
 """
 import os
@@ -17,14 +20,11 @@ import nibabel as nib
 from skimage.morphology import watershed
 from skimage.feature import peak_local_max
 from scipy import ndimage as ndi
-
-from ibc_public.utils_data import (
-    data_parser, SMOOTH_DERIVATIVES, SUBJECTS, LABELS, CONTRASTS,
-    CONDITIONS, THREE_MM, DERIVATIVES)
+from ibc_public.utils_data import (data_parser, SMOOTH_DERIVATIVES, SUBJECTS, LABELS, CONTRASTS, CONDITIONS, THREE_MM, DERIVATIVES)
 import ibc_public
 
-
-cache = '/neurospin/tmp/bthirion'
+# cache = '/neurospin/tmp/bthirion'
+cache = '/volatile/lemaitre/IBC'
 
 # caching
 mem = Memory(cachedir=cache, verbose=0, bytes_limit=4.e9)
@@ -34,12 +34,13 @@ write_dir = cache
 if not os.path.exists(write_dir):
     os.mkdir(write_dir)
 
-# mask of the grey  matter across subjects
+# mask of grey matter across subjects
 package_directory = os.path.dirname(
     os.path.abspath(ibc_public.utils_data.__file__))
 mask_gm = os.path.join(
     package_directory, '../ibc_data', 'gm_mask_1_5mm.nii.gz')
 
+#  select task fMRI
 subject_list = SUBJECTS
 task_list = ['rsvp_language']
 df = data_parser(derivatives=SMOOTH_DERIVATIVES, subject_list=SUBJECTS,
@@ -48,7 +49,7 @@ df = df[df.acquisition == 'ffx']
 conditions = df[df.modality == 'bold'].contrast.unique()
 n_conditions = len(conditions)
 
-
+# function for conjunction
 def conjunction_img(imgs, masker, percentile=50, threshold=3.1,
                     height_control='none'):
     """ generate conjunction statsitics of the contrat images in dataframe
@@ -77,15 +78,19 @@ def conjunction_img(imgs, masker, percentile=50, threshold=3.1,
     conj = conj * (np.abs(conj) > threshold)
     return conj_img, threshold_, conj
 
+# define parameters
 contrast = 'sentence-jabberwocky'
 imgs = df[df.contrast == contrast].path.values
-
 percentile = 50
 zmaps = []
 masker = NiftiMasker(mask_img=mask_gm).fit()
+
+# conjunctions analysis
 conj_img, threshold_, conj = conjunction_img(
     imgs, masker, percentile=percentile, threshold=.05,
     height_control='fdr')
+
+# display and save T map
 fname = os.path.join(write_dir, 'conj50_%s.png' % contrast)
 plotting.plot_glass_brain(conj_img, vmax=10, title=contrast)
 plotting.plot_glass_brain(conj_img, vmax=10, title=contrast,
@@ -93,6 +98,7 @@ plotting.plot_glass_brain(conj_img, vmax=10, title=contrast,
 fname = os.path.join(write_dir, 'conj50_%s.nii.gz' % contrast)
 conj_img.to_filename(fname)
 
+# display and save label map
 values = conj_img.get_data()
 local_maxi = peak_local_max(values, min_distance=5, indices=False)
 markers = ndi.label(local_maxi)[0]
@@ -105,4 +111,5 @@ plotting.plot_glass_brain(label_img, title='extracted regions',
 fname = os.path.join(write_dir, 'language_rois.nii.gz')
 label_img.to_filename(fname)
 
+# close display
 plt.show(block=False)
